62e01ff44dda28425f1eb33f3d14375822c91065
Ehtasham-ul-Haq <ehtashamul.haq@nmxglobalsoftware.com>
2019-07-18 01:08:06 +0500

    Added styling classes

--- client/src/components/table/table.jsx
+++ client/src/components/table/table.jsx
@@ -5,7 +5,7 @@ import Row from './table-row'
 
 const Table = ({ headers, data, resolvers }) => {
     return data ? (
-        <table className="table">
+        <table className="table is-bordered is-striped">
             {headers && <thead><tr>{headers.map((columnTitle, i) => <th key={i}>{columnTitle}</th>)}</tr></thead>}
             <tbody>{data.map((row, i) => <Row key={i} data={row} resolvers={resolvers}></Row>)}</tbody>
         </table>
