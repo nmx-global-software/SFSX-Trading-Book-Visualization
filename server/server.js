e2571d2d7c038615a0cfdbe1deaf1ec9e2e91eed
Ehtasham-ul-Haq <ehtashamul.haq@nmxglobalsoftware.com>
2019-07-17 20:46:49 +0500

    Added errorHandler

--- server/server.js
+++ server/server.js
@@ -4,8 +4,15 @@ const bodyParser = require('body-parser')
 const { PORT = 3000 } = process.env
 
 const app = express()
+
+const errorHandler = (error, req, res, next) => {
+    res.status(500).send({ error })
+}
+
 app.use(bodyParser.json())
 app.use('/book', require('./routers/book'))
+
+app.use(errorHandler)
 
 app.get('/', (req, res) => res.send("SFSX server is up!"))
 
