590066619452fabddd1884b3cd1fcf4b0ff2e383
Ehtasham-ul-Haq <ehtashamul.haq@nmxglobalsoftware.com>
2019-07-18 12:38:02 +0500

    Chart container now passing chartOptions to column chart

--- client/src/containers/chart-container.js
+++ client/src/containers/chart-container.js
@@ -6,8 +6,115 @@ import ColumnChart from '../components/charts/column-chart';
 
 const mapStateToProps = state => {
   return {
-    chart: { test: "data" },
-
+    chartOptions: {
+      chart: {
+        type: "column",
+        events: {
+          click: (e) => console.log(e.yAxis[0].value)
+        }
+      },
+      title: {
+        text: ""
+      },
+      xAxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec"
+        ]
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: "Rainfall (mm)"
+        }
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: [
+        {
+          name: "Tokyo",
+          data: [
+            49.9,
+            71.5,
+            106.4,
+            129.2,
+            144.0,
+            176.0,
+            135.6,
+            148.5,
+            216.4,
+            194.1,
+            95.6,
+            54.4
+          ]
+        },
+        {
+          name: "New York",
+          data: [
+            83.6,
+            78.8,
+            98.5,
+            93.4,
+            106.0,
+            84.5,
+            105.0,
+            104.3,
+            91.2,
+            83.5,
+            106.6,
+            92.3
+          ]
+        },
+        {
+          name: "London",
+          data: [
+            48.9,
+            38.8,
+            39.3,
+            41.4,
+            47.0,
+            48.3,
+            59.0,
+            59.6,
+            52.4,
+            65.2,
+            59.3,
+            51.2
+          ]
+        },
+        {
+          name: "Berlin",
+          data: [
+            42.4,
+            33.2,
+            34.5,
+            39.7,
+            52.6,
+            75.5,
+            57.4,
+            60.4,
+            47.6,
+            39.1,
+            46.8,
+            51.1
+          ]
+        }
+      ]
+    }
   }
 }
 
