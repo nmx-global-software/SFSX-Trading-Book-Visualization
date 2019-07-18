a748a4e0ea8cf1ac0fe21f8cc0c70e39f84c9a50
Ehtasham-ul-Haq <ehtashamul.haq@nmxglobalsoftware.com>
2019-07-18 17:17:47 +0500

    Hiding the chart if no data available

--- client/src/components/charts/column-chart.js
+++ client/src/components/charts/column-chart.js
@@ -7,8 +7,15 @@ import { createColumnChartConfig } from './chart-factory';
 export default class ColumnChart extends React.Component {
 
   render() {
+    const { buy, sell } = this.props
+
+    console.log({buy, sell});
+
+
+    if(!buy.length && !sell.length) return null
+
     const { chartOptions } = createColumnChartConfig(this.props);
- 
+
     return (
       <div>
         <HighchartsReact highcharts={Highcharts} options={chartOptions} />
