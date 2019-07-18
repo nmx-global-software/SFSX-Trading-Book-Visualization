85b7b8c947931eb4448cb5d09378bf22bd2d91bb
Nouman Waheed <nwaheed@presidio.com>
2019-07-18 12:57:08 +0500

    fixed page makrup

--- client/src/views/home/home.jsx
+++ client/src/views/home/home.jsx
@@ -1,61 +1,64 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
-import TradingFormContainer from '../../containers/trading-form-container';
-import HistoryContainer from '../../containers/history-container.js'
+import TradingFormContainer from "../../containers/trading-form-container";
+import HistoryContainer from "../../containers/history-container.js";
 import ColumnChartContainer from "../../containers/chart-container";
 
+import wrapper from "../../components/wrapper";
 
-import wrapper from '../../components/wrapper'
-
-import { loadTradersData } from '../../redux-ducks/traders/traders';
+import { loadTradersData } from "../../redux-ducks/traders/traders";
 
 import "./home.css";
 
-const Home = props=>{
+const Home = props => {
   return (
     <div className="main">
-    <div className="tile is-ancestor">
-      <div className="tile is-vertical is-8">
-        <div className="tile">
-          <div className="tile is-parent is-vertical">
-            <div className="tile is-child ">
-              <TradingFormContainer />
+      <div className="tile is-ancestor">
+        <div className="tile is-vertical is-8">
+          <div className="tile">
+            <div className="tile is-parent is-vertical">
+              <div className="tile is-child ">
+                <TradingFormContainer />
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child">
+                <ColumnChartContainer />
+              </div>
             </div>
           </div>
           <div className="tile is-parent">
-            <div className="tile is-child">
-              <ColumnChartContainer />
-            </div>
+            <article className="tile is-child notification is-danger">
+              <p className="title">Wide tile</p>
+              <p className="subtitle">Aligned with the right tile</p>
+              <div className="content" />
+            </article>
           </div>
         </div>
         <div className="tile is-parent">
-          <article className="tile is-child notification is-danger">
-            <p className="title">Wide tile</p>
-            <p className="subtitle">Aligned with the right tile</p>
-            <div className="content" />
-          </article>
-        </div>
-      </div>
-      <div className="tile is-parent">
-        <div className="tile is-child">
-          <HistoryContainer />
+          <div className="tile is-child">
+            <HistoryContainer />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
-  didMount:props =>{
-    loadTradersData(dispatch)()
+  didMount: props => {
+    loadTradersData(dispatch)();
   }
-})
+});
 
 const mergeProps = (stateProps, dispatchProps, ownProps) =>
-  Object.assign({}, stateProps, dispatchProps, ownProps)
+  Object.assign({}, stateProps, dispatchProps, ownProps);
 
-export const HomeEnhanced = connect(null, mapDispatchToProps, mergeProps)(wrapper(Home))
+export const HomeEnhanced = connect(
+  null,
+  mapDispatchToProps,
+  mergeProps
+)(wrapper(Home));
 
-export default HomeEnhanced
+export default HomeEnhanced;
