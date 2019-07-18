43542984dadf5f1cb93a7fcb3d799fa6120e4323
Ehtasham-ul-Haq <ehtashamul.haq@nmxglobalsoftware.com>
2019-07-18 12:22:31 +0500

    Mapping of history with dynamic data

--- client/src/containers/history-container.js
+++ client/src/containers/history-container.js
@@ -6,18 +6,9 @@ import Table from '../components/table/table'
 const resolvers = [row => row.timestamp, row => row.ticker, row => row.price, row => row.numberOfShares]
 const headers = ["Timestamp", "Ticker", "Price", "Share"]
 
-const TableWrapper = ({data}) => <Table resolvers={resolvers} headers={headers} data={data} />
+const TableWrapper = ({ data }) => <Table resolvers={resolvers} headers={headers} data={data} />
 
-const mapStateToProps = state => ({
-    data: [
-        {
-            "timestamp": "2019-07-17T16:55:49.416Z",
-            "ticker": "GOOG",
-            "price": 140,
-            "numberOfShares": 10
-        }
-    ]
-})
+const mapStateToProps = ({ traders }) => ({ data: traders.executionHistory })
 
 export const HistoryTableContainer = connect(mapStateToProps)(TableWrapper);
 export default HistoryTableContainer
